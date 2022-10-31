7c7890fda98ee3f76bbd6ee4742bde7b8d880e33
mouna <mounakumarswamy@mirafra.com>
2022-10-31 09:56:49 +0530

    code added

--- sequence_driver_handshake/driver.sv
+++ sequence_driver_handshake/driver.sv
@@ -15,15 +15,18 @@ endfunction
 virtual task run_phase(uvm_phase phase); 
 trans = transaction::type_id::create("trans");
 forever begin
-//get an item from sequencer using get_next method
-
 `uvm_info("Driver", "Waiting for data from sequencer", UVM_NONE);
+
+//get an item from sequencer using get_next method
+//Instead of get_next_item, we can use get(req)
 seq_item_port.get_next_item(req);
 `uvm_info("Driver", $sformatf("Start driving data, a : %0d b:%0d", req.a, req.b), UVM_NONE);
 
 //call the item_done method to send the request item back to sequencer
+//Instead of item_done, we can use put(req)
 seq_item_port.item_done();     
 `uvm_info("Driver", $sformatf("Finish driving data, a : %0d b:%0d", req.a, req.b), UVM_NONE);
+
 end
 endtask
 
