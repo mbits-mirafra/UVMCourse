8c6794bca45303b364766b4ccf03008109cdbfaf
VINUTHNA-SRI <113417505+VINUTHNA-SRI@users.noreply.github.com>
2023-01-07 11:45:04 +0530

    Update uvm_object_con2str_tb.sv

--- basics/obj_print/uvm_object_con2str_tb.sv
+++ basics/obj_print/uvm_object_con2str_tb.sv
@@ -3,20 +3,23 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 class test extends uvm_test;
-  `uvm_component_utils(test)
+   `uvm_component_utils(test)
 
-  function new(string name="test", uvm_component parent=null);
-    super.new(name,parent);
-  endfunction
+   function new(string name="test", uvm_component parent=null);
+      super.new(name,parent);
+   endfunction
 
-  function void build;
-    example Ex = example::type_id::create("Ex");
-    `uvm_info(get_type_name(),$sformatf("Contents:%s",Ex.convert2string()),UVM_LOW)
-  endfunction
+   function void build;
+      example Ex = example::type_id::create("Ex");
+      `uvm_info(get_type_name(),$sformatf("Contents:%s",Ex.convert2string()),UVM_LOW)
+   endfunction
+
 endclass
 
-module uvm_object_conv2str_tb;
-initial begin
-  run_test("test");
-end
+module uvm_object_con2str_tb;
+   test t;
+   initial begin
+      t=test::type_id::create("t",null);
+      t.build();
+   end
 endmodule
