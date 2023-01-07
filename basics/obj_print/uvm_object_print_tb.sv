76058afeb0f0d5937f964de457b051ade1ae6e4b
VINUTHNA-SRI <113417505+VINUTHNA-SRI@users.noreply.github.com>
2023-01-07 11:31:27 +0530

    Update uvm_object_print_tb.sv

--- basics/obj_print/uvm_object_print_tb.sv
+++ basics/obj_print/uvm_object_print_tb.sv
@@ -3,20 +3,24 @@ import uvm_pkg::*;
 `include "uvm_macros.svh"
 
 class test extends uvm_test;
-  `uvm_component_utils(test)
+   
+   `uvm_component_utils(test)
 
-  function new(string name="test", uvm_component parent=null);
-    super.new(name,parent);
-  endfunction
+   function new(string name="test", uvm_component parent=null);
+      super.new(name,parent);
+   endfunction
 
-  function void build;
-    example Ex = example::type_id::create("Ex");
-    Ex.print();
-  endfunction
+   function void build;
+      example Ex = example::type_id::create("Ex");
+      Ex.print();
+   endfunction
+
 endclass
 
 module uvm_object_print_tb;
-initial begin
-  run_test("test");
-end
+   test t;
+   initial begin
+      t=test::type_id::create("t",null);
+      t.build();
+   end
 endmodule
